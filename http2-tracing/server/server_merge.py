bbc12601dd2c5fd59cab36600009b24b62d991af
yhsmer <yhsm3er@gmail.com>
2022-08-07 23:22:48 +0800

    style: format python code with vscode formater

--- http2-tracing/server/server_merge.py
+++ http2-tracing/server/server_merge.py
@@ -12,7 +12,10 @@ bpf = BPF(text=bpf_source)
 # 服务端此函数接受明文header字段并将它们发送到内部缓冲区。函数签名和参数的类型定义是稳定的，自2018 年以来没有改变。
 # 任务是读取第三个参数的内容hf，它是HeaderField. 我们使用dlv调试器计算出嵌套数据元素的偏移量，从堆栈中读取数据
 
-bpf.attach_uprobe(name="./grpc_server", sym="google.golang.org/grpc/internal/transport.(*loopyWriter).writeHeader", fn_name="probe_loopy_writer_write_header")
+bpf.attach_uprobe(
+    name="./grpc_server",
+    sym="google.golang.org/grpc/internal/transport.(*loopyWriter).writeHeader",
+    fn_name="probe_loopy_writer_write_header")
 
 # 跟踪在 gRPC 服务端收到的传入标头，operateHeaders 解析 Headers 帧
 # Probe for the golang.org/x/net/http2 library's header reader (server-side).
@@ -24,23 +27,29 @@ bpf.attach_uprobe(name="./grpc_server", sym="google.golang.org/grpc/internal/tra
 # Symbol:
 #   google.golang.org/grpc/internal/transport.(*http2Server).operateHeaders
 
-bpf.attach_uprobe(name="./grpc_server", sym="google.golang.org/grpc/internal/transport.(*http2Server).operateHeaders", fn_name="probe_http2_server_operate_headers")
+bpf.attach_uprobe(
+    name="./grpc_server",
+    sym=
+    "google.golang.org/grpc/internal/transport.(*http2Server).operateHeaders",
+    fn_name="probe_http2_server_operate_headers")
 
 # Verified to be stable from at least go1.6 to t go.1.13.
 # Probe for the hpack's header encoder.
 #
 # Function signature:
 #   func (e *Encoder) WriteField(f HeaderField) error
-# 
-#   WriteField encodes f into a single Write to e's underlying Writer. 
+#
+#   WriteField encodes f into a single Write to e's underlying Writer.
 #   This function may also produce bytes for "Header Table Size Update" if necessary.
 #   If produced, it is done before encoding f.
 #   每次只处理header部分的一对key/value
-# 
+#
 # Symbol:
 #   golang.org/x/net/http2/hpack.(*Encoder).WriteField
 
-bpf.attach_uprobe(name="./grpc_server", sym="golang.org/x/net/http2/hpack.(*Encoder).WriteField", fn_name="probe_hpack_header_encoder")
+bpf.attach_uprobe(name="./grpc_server",
+                  sym="golang.org/x/net/http2/hpack.(*Encoder).WriteField",
+                  fn_name="probe_hpack_header_encoder")
 
 # Verified to be stable from at least go1.6 to t go.1.13.
 # func (fr *Framer) checkFrameOrder(f Frame) error
@@ -51,7 +60,9 @@ bpf.attach_uprobe(name="./grpc_server", sym="golang.org/x/net/http2/hpack.(*Enco
 # retprobe 主要用于探测函数返回值，以及计算函数耗时
 # read received data frame only
 
-bpf.attach_uprobe(name="./grpc_server", sym="golang.org/x/net/http2.(*Framer).checkFrameOrder", fn_name="probe_http2_framer_check_frame_order")
+bpf.attach_uprobe(name="./grpc_server",
+                  sym="golang.org/x/net/http2.(*Framer).checkFrameOrder",
+                  fn_name="probe_http2_framer_check_frame_order")
 
 # Verified to be stable from go1.7 to t go.1.13.
 # func (f *Framer) WriteDataPadded(streamID uint32, endStream bool, data, pad []byte) error
@@ -59,8 +70,9 @@ bpf.attach_uprobe(name="./grpc_server", sym="golang.org/x/net/http2.(*Framer).ch
 # Probe for the golang.org/x/net/http2 library's frame writer
 # WriteDataPadded writes a DATA frame with optional padding.
 
-bpf.attach_uprobe(name="./grpc_server", sym="golang.org/x/net/http2.(*Framer).WriteDataPadded", fn_name="probe_http2_framer_write_data")
-
+bpf.attach_uprobe(name="./grpc_server",
+                  sym="golang.org/x/net/http2.(*Framer).WriteDataPadded",
+                  fn_name="probe_http2_framer_write_data")
 
 output = 0
 if output:
@@ -70,21 +82,21 @@ else:
 
     def print_event(cpu, data, size):
         event = bpf["go_grpc_events"].event(data)
-        
+
         printb(b"%-20s %d" % (b"trace_role", event.trace_role))
         printb(b"%-20s %d" % (b"pid", event.pid))
         printb(b"%-20s %d" % (b"tgid", event.tgid))
 
-        if event.fd != 0 :
+        if event.fd != 0:
             printb(b"%-20s %d" % (b"fd", event.fd))
-        
-        if event.stream_id != 0 :
+
+        if event.stream_id != 0:
             printb(b"%-20s %d" % (b"stream_id", event.stream_id))
-        
+
         if event.remote_ip != 0:
             printb(b"%-20s %d" % (b"remote_ip", event.remote_ip))
 
-        if event.remote_port != 0 :
+        if event.remote_port != 0:
             printb(b"%-20s %d" % (b"remote_port", event.remote_port))
 
         if len(event.content_type) != 0:
@@ -92,7 +104,7 @@ else:
 
         if len(event.req_method) != 0:
             printb(b"%-20s %s" % (b"req_method", event.req_method))
-        
+
         if len(event.req_path) != 0:
             printb(b"%-20s %s" % (b"req_path", event.req_path))
 
@@ -101,16 +113,17 @@ else:
 
         if event.req_body_size != 0:
             printb(b"%-20s %d" % (b"req_body_size", event.req_body_size))
-        
+
         if event.reqsp_body_size != 0:
             printb(b"%-20s %d" % (b"reqsp_body_size", event.reqsp_body_size))
-        
+
         if event.timestamp_ns != 0:
             printb(b"%-20s %d(ns)" % (b"timestamp_ns", event.timestamp_ns))
-        
-        if event.name_size != 0  and event.value_size != 0:
-            printb(b"%-20s %s" % (event.name_msg[: event.name_size], event.value_msg[: event.value_size]))
-        
+
+        if event.name_size != 0 and event.value_size != 0:
+            printb(b"%-20s %s" % (event.name_msg[:event.name_size],
+                                  event.value_msg[:event.value_size]))
+
         print('')
 
     # loop with callback to print_event
